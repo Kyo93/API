ca6ff52ef48597a89daabab65116d93af2b6a77e
= <=>
2020-05-25 15:36:19 +0700

    Test apiview

--- profiles_user/views.py
+++ profiles_user/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test APIview"""
+
+    def get(self, request, fomat=None):
+        """Return a list of APiview features"""
+        an_apiview = [
+            'User HTTP methods as function (get, post, patch, put, delete)'
+            'Nguyen Thiep Here'
+        ]
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
